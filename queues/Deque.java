5b4854ae492240147996ab4e8e30db3c3f7701ff
unicorn-io <tayalvishesh83@gmail.com>
2019-06-05 20:17:50 +0530

    Commented For Better Understanding :))

--- queues/Deque.java
+++ queues/Deque.java
@@ -1,3 +1,11 @@
+/**
+ *  @author unicorn-io
+ *  Deque Implementation
+ *  A deque is a sub-type of queue that allows 
+ *  Insertion and Removal at both the ends
+ *  The name deque is short for "Double Ended Queue"
+ */
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -6,30 +14,52 @@ public class Deque<Item> implements Iterable<Item> {
     private Node last;
     private int numberOfitems = 0;
 
+    /**
+     * The Data type Node that has next prev and current values
+     */
     private class Node {
         private Node next;
         private Node prev;
         private Item item;
     }
 
+    /**
+     * Constructor Initializes an Null DEQUE 
+     */
     public Deque() {
         first = null;
         last = null;
     }
 
+    /**
+     *            Checks Whether The DEQUE is empty
+     * 
+     * @return    bool "True" if EMPTY else "False"
+     */
     public boolean isEmpty() {
         return numberOfitems == 0;
     }
 
+    /**
+     *            Gives the Size of the current Deque
+     * 
+     * @return    The size of the Deque
+     */
     public int size() {
         return numberOfitems;
     }
 
+    /**
+     *                This function adds a new Item
+     *                In the Deque at the beginning
+     * 
+     * @param item    The item to be added at beginning
+     */
     public void addFirst(Item item) {
         if (item == null) {
             throw new IllegalArgumentException("Bad Input!");
         }
-        Node oldFirst = first;
+        Node oldFirst = first; // Acts Temporary Place Holder
         first = new Node();
         first.item = item;
         first.next = oldFirst;
@@ -42,11 +72,17 @@ public class Deque<Item> implements Iterable<Item> {
         numberOfitems++;
     }
 
+    /**
+     *                This function adds a new Item
+     *                In the Deque at the end.
+     * 
+     * @param item    The item to be added at the end.
+     */
     public void addLast(Item item) {
         if (item == null) {
             throw new IllegalArgumentException("Bad Input!");
         }
-        Node oldLast = last;
+        Node oldLast = last; // Acts Temporary Place Holder.
         last = new Node();
         last.prev = oldLast;
         last.next = null;
@@ -59,6 +95,11 @@ public class Deque<Item> implements Iterable<Item> {
         numberOfitems++;
     }
 
+    /**
+     *            Removes The element at the Beginning
+     * 
+     * @return    The item that was removed from beginning  
+     */
     public Item removeFirst() {
         if (isEmpty()) {
             throw new NoSuchElementException("cha!!");
@@ -76,6 +117,11 @@ public class Deque<Item> implements Iterable<Item> {
         return item;
     }
 
+    /**
+     *            Removes The element at the Last
+     * 
+     * @return    The item that was removed from the Last.  
+     */
     public Item removeLast() {
         if (isEmpty()) { throw new NoSuchElementException("cha!!");
         }
@@ -91,6 +137,11 @@ public class Deque<Item> implements Iterable<Item> {
         return item;
     }
 
+    /**
+     * This lines of Code returns an Iterator
+     * That will allow us to use the for-each loop
+     * And adds functionality for easy access.
+     */
     public Iterator<Item> iterator() {
         return new DequeIterator();
     }
